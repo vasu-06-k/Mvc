18912b8f7298a92518435ec6b9d2aff512cc4daf
vasu-06-k <93372357+vasu-06-k@users.noreply.github.com>
2021-11-16 21:26:24 +0530

    Add files via upload

---
+++ LogindController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using HRBAL;
+using HRDAL;
+
+namespace Login.Controllers
+
+{
+    
+    public class LogindController : ApiController
+    {
+
+        //[HttpPost]
+
+        //public IHttpActionResult LoginAdmin(login L)
+        //{
+        //    LoginDAL dal = new LoginDAL();
+        //    int status = dal.ValidateUser(L);
+
+        //    if (status == 0)
+        //    {
+        //        return Ok(new { status = 401, isSuccess = false, message = "Invalid User", });
+        //    }
+        //    else
+        //    {
+        //        return Ok(new { status = 200, isSuccess = true, message = "User Login successfully" });
+        //    }
+        //}
+
+        //[HttpPost]
+
+        //public IHttpActionResult insert([FromBody]Employeedetails L)
+        //{
+        //    LoginDAL dal = new LoginDAL();
+        //     dal.add(L);
+
+           
+            
+        //        return Ok(new { status = 200, isSuccess = true, message = "Employee added successfully" });
+            
+        //}
+        [HttpPost]
+
+        public IHttpActionResult updates(int i,[FromBody] Employeedetails L)
+        {
+            LoginDAL dal = new LoginDAL();
+            dal.update(i,L);
+
+
+
+            return Ok(new { status = 200, isSuccess = true, message = "Employee updated successfully" });
+
+        }
+
+
+    }
+}
+
