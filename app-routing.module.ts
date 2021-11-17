b4d2e348364a4db6102d3cf898880730ee34e41a
vasu-06-k <93372357+vasu-06-k@users.noreply.github.com>
2021-11-17 18:29:13 +0530

    Routing module for hr project

--- app-routing.module.ts
+++ app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddemployeeComponent } from './addemployee/addemployee.component';
 import { EmployeeComponent } from './employee/employee.component';
+import { HomeComponent } from './home/home.component';
 import { LoginmComponent } from './loginm/loginm.component';
+import { UpdateemployeeComponent } from './updateemployee/updateemployee.component';
 LoginmComponent
 
 const routes: Routes = [ {path:'LoginPage',component:LoginmComponent},
-{path:'employee',component:EmployeeComponent}
+{path:'employee',component:EmployeeComponent},
+{path:'home',component:HomeComponent},
+{path:'addemployee',component:AddemployeeComponent},
+{path:'updateemployee',component:UpdateemployeeComponent}
 ];
 export default routes;
 
