c4a86792f51754b14d22d464150405f579b225ba
vasu-06-k <93372357+vasu-06-k@users.noreply.github.com>
2021-11-20 09:18:59 +0530

    Add files via upload

---
+++ PayrollController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using HRBAL;
+using HRDAL;
+
+namespace Login.Controllers
+{
+    public class PayrollController : ApiController
+    {
+        [HttpGet] //display particular employee
+
+        public Payroll displayupdate(int id)
+        {
+           payrollDAL  dal = new payrollDAL();
+            Payroll e1 = dal.displaypayroll(id);
+            return e1;
+
+
+
+
+        }
+        [HttpPut]
+        
+        public IHttpActionResult updates([FromBody] Payroll L)
+        {
+          payrollDAL  dal = new payrollDAL();
+            dal.updatedetails(L);
+
+
+
+            return Ok(new { status = 200, isSuccess = true, message = "Details Added successfully" });
+
+        }
+
+    }
+}
---
+++ payrollDAL.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HRBAL;
+
+namespace HRDAL
+{
+    public class payrollDAL
+    {
+        public Payroll displaypayroll(int id)
+        {
+            SqlConnection cn = new SqlConnection("Data Source=LAPTOP-IU1NK3H4\\SQLEXPRESS;Initial Catalog=HRdb;Integrated Security=True");
+
+            SqlDataAdapter d = new SqlDataAdapter("select * from PayRoll", cn);
+            d.MissingSchemaAction = MissingSchemaAction.AddWithKey;
+            DataSet t = new DataSet();
+            d.Fill(t);
+            
+            DataRow r=t.Tables[0].Rows.Find(id);
+
+
+            Payroll e2 = new Payroll();
+            //e2.Empid = Convert.ToInt32(r["EmpId"]);
+            e2.BankaccountNo = Convert.ToInt32(r["BankAccountNo"]);
+            //e2.Pfno = Convert.ToInt32(r["ProvidentFundNo"]);
+            e2.Basicpay = Convert.ToInt32(r["BasicPay"]);
+            e2.HRA = Convert.ToInt32(r["HRA"]);
+            e2.Medicalallowance = Convert.ToInt32(r["MedicalAllowance"]);
+            e2.Tax = Convert.ToInt32(r["Tax"]);
+            e2.Bonus = Convert.ToInt32(r["Bonus"]);
+            e2.Grosspay = Convert.ToInt32(r["GrossPay"]);
+            e2.Noofworkdays = Convert.ToInt32(r["NoOfWorkDays"]);
+            return e2;
+
+        }
+        public void updatedetails(Payroll e)
+        {
+            SqlConnection cn = new SqlConnection("Data Source=LAPTOP-IU1NK3H4\\SQLEXPRESS;Initial Catalog=HRdb;Integrated Security=True");
+
+            SqlDataAdapter d = new SqlDataAdapter("select * from PayRoll", cn);
+            d.MissingSchemaAction = MissingSchemaAction.AddWithKey;
+            DataSet t = new DataSet();
+            d.Fill(t);
+            //d.MissingSchemaAction = MissingSchemaAction.AddWithKey;
+            DataRow r = t.Tables[0].Rows.Find(e.Empid);
+            r[1] = e.BankaccountNo;
+            r[2] = e.Pfno;
+            
+
+            SqlCommandBuilder b = new SqlCommandBuilder(d);
+            d.Update(t);
+
+
+
+        }
+    }
+}
